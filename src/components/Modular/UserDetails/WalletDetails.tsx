498cdd80467be12d628e65f313fff9404de2e6e2
Sauli <sauli.savinainen@gmail.com>
2023-08-01 05:06:51 +0300

    update

--- src/components/Modular/UserDetails/WalletDetails.tsx
+++ src/components/Modular/UserDetails/WalletDetails.tsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Popover, Typography, IconButton } from '@mui/material';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInfoCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
-import styled, { keyframes } from 'styled-components';
-import { useConnect, useAccount, useDisconnect } from 'wagmi';
+import React, { useState, useEffect } from "react";
+import { Button, Popover, Typography, IconButton } from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle, faTimes } from "@fortawesome/free-solid-svg-icons";
+import styled, { keyframes } from "styled-components";
+import { useConnect, useAccount, useDisconnect } from "wagmi";
 
 const pulseAnimation = keyframes`
   0% {
@@ -17,12 +17,33 @@ const pulseAnimation = keyframes`
   }
 `;
 
+const greenPulseAnimation = keyframes`
+  0% {
+    box-shadow: 0 0 0 0 rgba(0, 255, 0, 0.7);
+  }
+  70% {
+    box-shadow: 0 0 0 10px rgba(0, 255, 0, 0);
+  }
+  100% {
+    box-shadow: 0 0 0 0 rgba(0, 255, 0, 0);
+  }
+`;
+
 const RedLight = styled.div`
   width: 12px;
   height: 12px;
   border-radius: 50%;
   background-color: red;
   animation: ${pulseAnimation} 2s infinite;
+  margin-right: 10px;
+`;
+
+const GreenLight = styled.div`
+  width: 12px;
+  height: 12px;
+  border-radius: 50%;
+  background-color: green;
+  animation: ${greenPulseAnimation} 2s infinite;
   margin-right: 10px;
 `;
 
@@ -44,7 +65,7 @@ const InfoButton: React.FC = () => {
       <IconButton
         onClick={handlePopoverOpen}
         size="small"
-        sx={{ color: 'white', marginRight: '0.2rem', marginTop: '0.2rem' }}
+        sx={{ color: "white", marginRight: "0.2rem", marginTop: "0.2rem" }}
       >
         <FontAwesomeIcon icon={faInfoCircle} />
       </IconButton>
@@ -53,18 +74,18 @@ const InfoButton: React.FC = () => {
         anchorEl={anchorEl}
         onClose={handlePopoverClose}
         anchorOrigin={{
-          vertical: 'center',
-          horizontal: 'right',
+          vertical: "center",
+          horizontal: "right",
         }}
         transformOrigin={{
-          vertical: 'center',
-          horizontal: 'left',
+          vertical: "center",
+          horizontal: "left",
         }}
       >
         <IconButton
           aria-label="close"
           size="small"
-          style={{ position: 'absolute', top: 5, right: 5 }}
+          style={{ position: "absolute", top: 5, right: 5 }}
           onClick={handlePopoverClose}
         >
           <FontAwesomeIcon icon={faTimes} />
@@ -80,9 +101,10 @@ const InfoButton: React.FC = () => {
 };
 
 const WalletDetails: React.FC = () => {
-  const { connect, connectors, error, isLoading, pendingConnector } = useConnect();
+  const { connect, connectors, error, isLoading, pendingConnector } =
+    useConnect();
   const { address, connector, isConnected } = useAccount();
-  const { disconnect } = useDisconnect()
+  const { disconnect } = useDisconnect();
 
   // Function to connect wallet
   const connectWallet = async () => {
@@ -114,11 +136,12 @@ const WalletDetails: React.FC = () => {
     }
   }, [isConnected, address]);
 
-
   return (
     <div className="flex ml-2 justify-between">
       <div className="flex items-center mr-2 text-sm allerta-stencil">
-        <RedLight /> Wallet not connected!
+        {isConnected ? <GreenLight /> : <RedLight />}{" "}
+        {/* Conditionally render RedLight or GreenLight */}
+        {isConnected ? "Wallet connected!" : "Wallet not connected!"}
       </div>
       {isConnected ? (
         <Button
@@ -126,26 +149,26 @@ const WalletDetails: React.FC = () => {
           color="primary"
           onClick={disconnectWallet}
           sx={{
-            color: 'white',
-            border: '1px solid white',
-            background: 'transparent',
-            fontFamily: 'Audiowide',
-            padding: '0.4rem 0.45rem',
-            margin: '0.45rem',
-            display: 'flex',
-            alignItems: 'center',
-            '&:hover': {
-              color: '#1976d2',
-              border: '1px solid #1976d2',
-              background: 'transparent',
+            color: "white",
+            border: "1px solid white",
+            background: "transparent",
+            fontFamily: "Audiowide",
+            padding: "0.4rem 0.45rem",
+            margin: "0.45rem",
+            display: "flex",
+            alignItems: "center",
+            "&:hover": {
+              color: "#1976d2",
+              border: "1px solid #1976d2",
+              background: "transparent",
             },
-            '&:focus': {
-              color: '#1976d2',
-              border: '1px solid #1976d2',
+            "&:focus": {
+              color: "#1976d2",
+              border: "1px solid #1976d2",
             },
-            '&:active': {
-              color: '#1976d2',
-              border: '1px solid #1976d2',
+            "&:active": {
+              color: "#1976d2",
+              border: "1px solid #1976d2",
             },
           }}
         >
@@ -157,33 +180,31 @@ const WalletDetails: React.FC = () => {
           color="primary"
           onClick={connectWallet}
           sx={{
-            color: 'white',
-            border: '1px solid white',
-            background: 'transparent',
-            fontFamily: 'Audiowide',
-            padding: '0.4rem 0.45rem',
-            margin: '0.45rem',
-            display: 'flex',
-            alignItems: 'center',
-            '&:hover': {
-              color: '#1976d2',
-              border: '1px solid #1976d2',
-              background: 'transparent',
+            color: "white",
+            border: "1px solid white",
+            background: "transparent",
+            fontFamily: "Audiowide",
+            padding: "0.4rem 0.45rem",
+            margin: "0.45rem",
+            display: "flex",
+            alignItems: "center",
+            "&:hover": {
+              color: "#1976d2",
+              border: "1px solid #1976d2",
+              background: "transparent",
             },
-            '&:focus': {
-              color: '#1976d2',
-              border: '1px solid #1976d2',
+            "&:focus": {
+              color: "#1976d2",
+              border: "1px solid #1976d2",
             },
-            '&:active': {
-              color: '#1976d2',
-              border: '1px solid #1976d2',
+            "&:active": {
+              color: "#1976d2",
+              border: "1px solid #1976d2",
             },
           }}
         >
           <InfoButton />
-          <span style={{ flex: 1, marginTop: '4px' }}>
-            Connect Wallet
-          </span>
+          <span style={{ flex: 1, marginTop: "4px" }}>Connect Wallet</span>
         </Button>
       )}
     </div>
