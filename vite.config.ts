22fc7196ad78b5a549b9031b3a5e6b819057ef26
Sauli <sauli.savinainen@gmail.com>
2023-08-01 04:57:24 +0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,25 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    global: 'globalThis',
-  },
+    global: (() => {
+      let globalVariable = 'globalThis';
+      try {
+        // Try to import @safe-global/safe-apps-provider
+        require.resolve('@safe-global/safe-apps-provider');
+        // Try to import @safe-global/safe-apps-sdk
+        require.resolve('@safe-global/safe-apps-sdk');
+        // If both modules are found, return the custom global variable
+        globalVariable = 'global';
+       } catch (e) {
+        // If either module is not found, fallback to globalThis
+        globalVariable = 'globalThis';
+       }
+      return globalVariable;
+     })()
+   },
   resolve: {
     alias: {
       process: 'process/browser',
@@ -13,4 +27,4 @@ export default defineConfig({
     },
   },
   plugins: [react()],
-})
+});
